9e8bd99d880e8204d00050399ce3d800c1a6db32
rustamaha <ruslyandiya@mail.ru>
2019-10-28 18:35:13 +0300

    'second_step'

--- __tests__/test.js
+++ __tests__/test.js
@@ -5,59 +5,102 @@ import path from 'path';
 import { promises as fs } from 'fs';
 
 import pageLoader from '../src';
-import { makeFileName } from '../src/loader';
+import { makeName } from '../src/loader';
 
-const url1 = 'https://hexlet.io/courses';
-const url2 = 'https://yandex.ru/pogoda/saint-petersburg?lat=59.957932&lon=30.298605';
+const url1 = 'https://ru.hexlet.io/courses';
 
 const buildPathForFixture = (format, fileName) => path.join('__tests__', '__fixtures__', `${fileName}${format}`);
-
-const hexlet = buildPathForFixture('.html', 'hexlet');
-const yandexPogoda = buildPathForFixture('.html', 'yandex-pogoda');
-
+const buildPathForHexletFiles = (format, fileName) => path.join('__tests__', '__fixtures__', 'ru-hexlet-io-courses_files', `${fileName}${format}`);
+const originalHexletPage = buildPathForFixture('.html', 'view-source_ru.hexlet.io_courses');
+const convertedHexletPage = buildPathForFixture('.html', 'ru-hexlet-io-courses');
+const cssFile = buildPathForHexletFiles('.css', 'cdn2-hexlet-io-assets-application-58b8be69d43878d8ffa548a26a341422323098508999ea2cd5f001896ad189dc-css');
+let tempDir;
+//  let localFilesDir;
+let filePath;
+let cssFilePath;
 let expected1;
 let expected2;
-let pathDir;
-let filePath1;
-let filePath2;
 
 beforeAll(() => {
-  const hexletFile = makeFileName(url1, '.html');
-  const yandexFile = makeFileName(url2, '.html');
+  const hexletFile = makeName(url1, '.html');
+  const localFiles = makeName(url1, '_files');
+  const cssFileName = 'cdn2-hexlet-io-assets-application-58b8be69d43878d8ffa548a26a341422323098508999ea2cd5f001896ad189dc-css.css';
   fs.mkdtemp(path.join(os.tmpdir()))
     .then((dir) => {
-      pathDir = dir;
-      filePath1 = path.join(dir, hexletFile);
-      filePath2 = path.join(dir, yandexFile);
+      tempDir = dir;
+      filePath = path.resolve(dir, hexletFile);
+      //  localFilesDir = path.resolve(dir, localFiles);
+      cssFilePath = path.resolve(dir, localFiles, cssFileName);
+      return fs.readFile(convertedHexletPage, 'utf8');
+    })
+    .then((data) => {
+      expected1 = data;
+    })
+    .then(() => fs.readFile(cssFile, 'utf8'))
+    .then((data) => {
+      expected2 = data;
     });
 });
 
-test('a page from url1 is successfully downloaded and saved', async () => {
-  await nock('https://hexlet.io')
+test('a page from url1 is successfully converted and saved local files', async () => {
+  await nock('https://ru.hexlet.io')
     .get('/courses')
-    .reply(200, () => fs.readFile(hexlet, 'utf8')
-      .then((data) => {
-        expected1 = data;
-      }));
+    .reply(200, () => fs.readFile(originalHexletPage, 'utf8'));
+  await nock('https://ru.hexlet.io')
+    .get('/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js')
+    .reply(200, {
+      data: 'hello world',
+    });
+  await nock('https://cdn2.hexlet.io')
+    .get('/assets/application-18fceb6e3c2174543bef135dd059ef4b818426e5c00c6e8d320344d7c96a2c00.js')
+    .reply(200, {
+      data: 'hello world',
+    });
+  await nock('https://cdn2.hexlet.io')
+    .get('/packs/js/runtime~application-afec33d14cb7ead791f8.js')
+    .reply(200, {
+      data: 'hello world',
+    });
+  await nock('https://cdn2.hexlet.io')
+    .get('/packs/js/1-690958a8287b31a22ef2.chunk.js')
+    .reply(200, {
+      data: 'hello world',
+    });
+  await nock('https://cdn2.hexlet.io')
+    .get('/packs/js/34-ea12df81beb39d7f7cd6.chunk.js')
+    .reply(200, {
+      data: 'hello world',
+    });
+  await nock('https://cdn2.hexlet.io')
+    .get('/packs/js/application-386fb49119b4a3c6179e.chunk.js')
+    .reply(200, {
+      data: 'hello world',
+    });
+  await nock('https://cdn2.hexlet.io')
+    .get('/assets/application-58b8be69d43878d8ffa548a26a341422323098508999ea2cd5f001896ad189dc.css')
+    .reply(200, () => fs.readFile(cssFile, 'utf8'));
+  await nock('https://cdn2.hexlet.io')
+    .get('/packs/css/34-e1004b13.chunk.css')
+    .reply(200, {
+      data: 'hello world',
+    });
+  await nock('https://ru.hexlet.io')
+    .get('/lessons.rss')
+    .reply(200, {
+      data: 'hello world',
+    });
+  await nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, {
+      data: 'hello world',
+    });
 
-  await pageLoader(url1, pathDir);
+  await pageLoader(url1, tempDir);
+
   setTimeout(async () => {
-    await fs.readFile(filePath1, 'utf8')
+    await fs.readFile(filePath, 'utf8')
       .then(data => expect(data).toMatch(expected1));
-  }, 5000);
-});
-
-test('a page from url2 is successfully downloaded and saved', async () => {
-  await nock('https://yandex.ru')
-    .get('/pogoda/saint-petersburg?lat=59.957932&lon=30.298605')
-    .reply(200, () => fs.readFile(yandexPogoda, 'utf8')
-      .then((data) => {
-        expected2 = data;
-      }));
-
-  await pageLoader(url2, pathDir);
-  setTimeout(async () => {
-    await fs.readFile(filePath2, 'utf8')
+    await fs.readFile(cssFilePath, 'utf8')
       .then(data => expect(data).toMatch(expected2));
   }, 5000);
 });
--- src/loader.js
+++ src/loader.js
@@ -3,29 +3,132 @@ import httpAdapter from 'axios/lib/adapters/http';
 import path from 'path';
 
 import { promises as fs } from 'fs';
-import _ from 'lodash';
+import cheerio from 'cheerio';
+import os from 'os';
 
 axios.defaults.adapter = httpAdapter;
 
-export const makeFileName = (url, type) => {
-  const { hostname, searchParams, pathname } = new URL(url);
-  const fileName = _.kebabCase(`${hostname}${pathname}-${searchParams}`);
-  return `${fileName}${type}`;
+export const makeName = (url, type) => {
+  const reProtocol = /^https:\/\/|^\/\/|^\//;
+  const re = /[\.,/,?,=]/g; //  eslint-disable-line
+  const name = url.replace(reProtocol, '').replace(re, '-');
+  return type ? `${name}${type}` : `${name}`;
 };
 
-export default (url, pathDir = '/') => {
-  const fileName = makeFileName(url, '.html');
+//  const url1 = 'https://ru.hexlet.io/courses';
+
+
+const pageLoader = (pageUrl, pathDir = '') => {
+  const {
+    hostname,
+    origin,
+  } = new URL(pageUrl);
+  const reSld = /(\.[^.]*)(\.*$)/;
+  const secLvlDomain = hostname.replace(reSld, '').split('.').pop();
+  //  console.log(secLvlDomain);
   let page;
-  const request = axios.get(url)
+  const paths = {};
+  const dirName = makeName(pageUrl, '_files');
+
+  const request = axios.get(pageUrl)
     .then(({ data }) => {
       page = data;
       return data;
     });
-  if (pathDir.length > 1) {
-    request
-      .then(() => fs.writeFile(path.resolve(pathDir, fileName), page));
-    return;
+  const dom = request.then((data) => {
+    const $ = cheerio.load(data);
+    return $;
+  });
+  const filter = ($, attr) => (i, el) => {
+    const pathEl = $(el).attr(attr);
+    const re1 = /^https:\/\/[a-z.0-9-]{2,}\.[a-z]{2,}|^\/\/[a-z.0-9-]{2,}\.[a-z]{2,}/;
+    const re2 = /^\/[a-z0-9-]{2,}/;
+    if (pathEl && pathEl.match(re1)) {
+      const matched = pathEl.match(re1);
+      const hostName = matched[0].slice(2).split('.');
+      const secLvlD = hostName[hostName.length - 2];
+      //  console.log('filter', pathEl, secLvlD, secLvlDomain);
+      return secLvlD === secLvlDomain;
+    }
+    return pathEl && pathEl.match(re2);
+  };
+  const traverse = ($, attr) => (i, el) => {
+    const pathEl = $(el).attr(attr);
+    //  console.log('path', pathEl, $.html(el));
+    const reUrlWithHttps = /^https:\/\/[a-z.0-9-]{2,}\.[a-z]{2,}/;
+    const reUrlWithoutHttps = /^\/\/[a-z.0-9-]{2,}\.[a-z]{2,}/;
+    const reType = /\.[a-z]{2,}$|\.[a-z]{2,}\/[a-z0-9]{2,}$/g;
+    const typeMatch = pathEl.match(reType);
+    let type;
+    if (typeMatch) {
+      const [ending] = typeMatch[0].split('/');
+      type = ending;
+    }
+    const fileName = makeName(pathEl, type);
+    $(el).attr({
+      src: `./${dirName}/${fileName}`,
+    });
+    page = $.html();
+    if (pathEl.match(reUrlWithHttps)) {
+      paths[pathEl] = { fileName, type };
+      return;
+    }
+    if (pathEl.match(reUrlWithoutHttps)) {
+      const makeUrl = `https:${pathEl}`;
+      paths[makeUrl] = { fileName, type };
+      return;
+    }
+    const makeUrl = `${origin}${pathEl}`;
+    paths[makeUrl] = { fileName, type };
+  };
+
+  const scripts = dom.then($ => $('script').filter(filter($, 'src'))
+    .map(traverse($, 'src')));
+  const links = dom.then($ => $('link').filter(filter($, 'href'))
+    .map(traverse($, 'href')));
+  const images = dom.then($ => $('img').filter(filter($, 'src'))
+    .map(traverse($, 'src')));
+
+  let promises;
+  const html = Promise.all([scripts, links, images])
+    //  .then(() => console.log(paths))
+    .then(() => {
+      const getData = Object.keys(paths).map((key) => {
+        const { type } = paths[key];
+        if (type === '.png') {
+          return axios.get(key, { responseType: 'arraybuffer' });
+        }
+        return axios.get(key);
+      });
+      promises = getData;
+    });
+  const pageName = makeName(pageUrl, '.html');
+  let pathToFiles;
+  if (pathDir.length > 0) {
+    const localFiles = path.resolve(pathDir, dirName);
+    html.then(() => fs.writeFile(path.resolve(pathDir, pageName), page))
+      .then(() => fs.mkdir(localFiles))
+      .then(() => {
+        pathToFiles = localFiles;
+        return pathToFiles;
+      });
+  } else {
+    html.then(() => fs.mkdtemp(path.join(os.tmpdir())))
+      .then((dir) => {
+        const localFiles = path.resolve(dir, dirName);
+        pathToFiles = localFiles;
+        return fs.writeFile(path.resolve(dir, pageName), page);
+      })
+      .then(() => fs.mkdir(pathToFiles));
   }
-  request
-    .then(() => fs.writeFile(path.resolve(pathDir, fileName), page));
+  html.then(() => axios.all(promises))
+    .then(axios.spread((...responses) => responses.forEach((response) => {
+      const { data } = response;
+      const { url } = response.config;
+      const { fileName } = paths[url];
+      fs.writeFile(path.resolve(pathToFiles, fileName), data);
+    })));
 };
+
+//  pageLoader(url1, 'C:\\');
+export default pageLoader;
